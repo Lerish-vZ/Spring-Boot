8edf4b3a4797305b6dd92f616fbfc050ab23801e
Lerish-vZ <lerishavz@gmail.com>
2024-03-13 11:54:38 +0200

    first commit

---
+++ taco-cloud-chp-01/src/main/java/tacos/TacoCloudChp01Application.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudChp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudChp01Application.class, args);
+	}
+
+}
